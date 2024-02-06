ca5031466b7e634a13809fce1cabbf4af7c81cab
yumikori <yumikori.biz@gmail.com>
2024-02-07 03:43:53 +0900

    ファイル構造を作成しなおした

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import 'phaser';
-import GameState from './game/GameState';
+// import 'phaser';
+// import GameState from './game/GameState';
 
-const config: Phaser.Types.Core.GameConfig = {
-  type: Phaser.AUTO,
-  width: 720,  // スマートフォンの縦向き解像度に合わせて設定
-  height: 1280, // スマートフォンの縦向き解像度に合わせて設定
-  scene: [GameState],
-};
+// const config: Phaser.Types.Core.GameConfig = {
+//   type: Phaser.AUTO,
+//   width: 720,  // スマートフォンの縦向き解像度に合わせて設定
+//   height: 1280, // スマートフォンの縦向き解像度に合わせて設定
+//   scene: [GameState],
+// };
 
-// Phaserゲームのインスタンスを作成
-const game = new Phaser.Game(config);
+// // Phaserゲームのインスタンスを作成
+// const game = new Phaser.Game(config);
